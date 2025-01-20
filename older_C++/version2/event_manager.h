b64d17b3af8227ace4f3ce9673cbb602b21c5cad
Weiyuan <128281190+WyOoi@users.noreply.github.com>
2025-01-21 04:00:31 +0800

    Create event_manager.h

---
+++ older_C++/version2/event_manager.h
@@ -0,0 +1,37 @@
+#ifndef EVENT_MANAGER_H
+#define EVENT_MANAGER_H
+
+#include "event_structs.h"
+#include "stack.h"
+#include "queue.h"
+#include "quicksort.h"
+
+class EventManager {
+public:
+    EventManager();
+    ~EventManager();
+
+    void addEvent();
+    void registerParticipant();
+    void displayAllEvents();
+    void searchEvent();
+    void sortEvents();
+    void undoLastAction();
+
+private:
+    Event* events;  // Array of events
+    Queue** eventQueues;  // Array of queues (one for each event)
+    int* waitingListSizes;  // Array to store number of participants for each event
+    int eventCount;  // Total number of events
+    int capacity;  // Maximum number of events the system can hold
+    Stack actionStack;  // Stack for undo functionality
+
+    void resizeArray();
+};
+
+void clearInputBuffer();
+bool adminLogin();
+void adminMode(EventManager &manager);
+void userMode(EventManager &manager);
+
+#endif // EVENT_MANAGER_H
